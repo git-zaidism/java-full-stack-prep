1d676e278410c441a4a84778d879b60db7346ceb
git-zaidisim <zaidisim7@gmail.com>
2024-02-01 20:05:09 +0530

    "remove demo class"

--- src/main/java/org/zaidism/temp/Main.java
+++ src/main/java/org/zaidism/temp/Main.java
@@ -3,16 +3,6 @@ package org.zaidism.temp;
 
 public class Main {
   public static void main(String[] args) {
-    String sentence = "coding test is mandatory";
-    String[] sentenceArr = sentence.split(" ");
-    String[] reverseArr = new String[sentenceArr.length];
-    int count = 0;
-    for (int i = sentenceArr.length - 1; i >= 0; i--) {
-      reverseArr[count] = sentenceArr[i];
-      count++;
-    }
 
-    String reversedSentence = String.join(" ", reverseArr);
-    System.out.println(reversedSentence);
   }
 }
