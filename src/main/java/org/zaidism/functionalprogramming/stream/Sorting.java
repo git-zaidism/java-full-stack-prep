fc700143a45e2aecf7e98e1976ccdd2b8aa2834a
git-zaidisim <zaidisim7@gmail.com>
2024-02-09 16:00:39 +0530

    "singleton pattern fixed"

--- src/main/java/org/zaidism/functionalprogramming/stream/Sorting.java
+++ src/main/java/org/zaidism/functionalprogramming/stream/Sorting.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 import static org.zaidism.model.EmployeeProvider.getEmployeeList;
 
-public class Sorted {
+public class Sorting {
   public static void main(String[] args) {
     List<Employee> employees = getEmployeeList();
     employees.stream().sorted((e1, e2) -> e1.getBirthDate().compareTo(e2.getBirthDate())).toList().forEach(System.out::println);
