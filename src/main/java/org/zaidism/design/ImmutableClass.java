0afd7838d14b79b02e4f68b0e60f42e816a95b13
git-zaidisim <zaidisim7@gmail.com>
2024-02-14 18:24:53 +0530

    " updated codes and text files"

--- src/main/java/org/zaidism/design/ImmutableClass.java
+++ src/main/java/org/zaidism/design/ImmutableClass.java
@@ -1,17 +1,27 @@
 package org.zaidism.design;
 
+import org.zaidism.model.Employee;
+
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public final class ImmutableClass {
 
   private final int id;
-  private final List<String> subject;
+  private final List<String> subject; // collection object immutability
 
+  private Employee employee; // custom object immutable
 
   public ImmutableClass(int id, List<String> subject) {
     this.id = id;
     this.subject = new ArrayList<>(subject);
+  }
+
+  public ImmutableClass(int id, List<String> subject, Employee employee) {
+    this.id = id;
+    this.subject = subject;
+    this.employee = employee;
   }
 
   public int getId() {
@@ -20,23 +30,35 @@ public final class ImmutableClass {
 
   public List<String> getSubject() {
     return new ArrayList<>(subject);
+    // return Collections.unmodifiableList(subject);
   }
 
-    @Override
-    public String toString() {
-        return "ImmutableClass{" +
-                "id=" + id +
-                ", subject=" + subject +
-                '}';
-    }
+  public Employee getEmployee() {
+    return new Employee(
+        employee.getId(),
+        employee.getName(),
+        employee.getBirthDate(),
+        employee.getSalary(),
+        employee.getTechStack());
+  }
+
+  @Override
+  public String toString() {
+    return "ImmutableClass{" + "id=" + id + ", subject=" + subject + ", employee=" + employee + '}';
+  }
 }
 
 class ImmutableTest {
   public static void main(String[] args) {
     List<String> subject = new ArrayList<>();
     subject.add("subject1");
-    ImmutableClass immutableClass = new ImmutableClass(1, subject);
-    immutableClass.getSubject().add("rr");
-    System.out.println(immutableClass);
+    ImmutableClass immutableClass1 = new ImmutableClass(1, subject);
+    immutableClass1.getSubject().add("rr");
+    System.out.println(immutableClass1);
+
+    Employee employee = new Employee(21, "Jahid", LocalDate.now(), 600000L, List.of("Java"));
+    ImmutableClass immutableClass2 = new ImmutableClass(1, subject, employee);
+    immutableClass2.getEmployee().setId(2000);
+    System.out.println(immutableClass2);
   }
 }
--- src/main/java/org/zaidism/interviewprograms/TwoSum.java
+++ src/main/java/org/zaidism/interviewprograms/TwoSum.java
@@ -1,6 +1,51 @@
 package org.zaidism.interviewprograms;
 
+// Given an array of N integers, and an integer K, the task is to find the number of pairs of
+// integers in the array whose sum is equal to K.
+// Input: arr[] = {1, 5, 7, -1, 5, 1, 2, 4} K = 6
+
+// Given an array of integers nums and an integer target, return indices of the two numbers such
+// that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same
+// element twice.
+// You can return the answer in any order.
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TwoSum {
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    int[] sum = {3, 2, 4};
+    int[] pair = twoSum(sum, 6);
+    System.out.println(Arrays.toString(pair));
+  }
+
+  static int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> numsMap = new HashMap<>();
+
+    for (int i = 0; i < nums.length; i++) numsMap.put(nums[i], i);
+
+    for (int i = 0; i < nums.length; i++) {
+      int num = nums[i];
+      int remainder = target - num;
+
+      if (numsMap.containsKey(remainder)) {
+
+        int index = numsMap.get(remainder);
+
+        if (index == i) continue;
+
+        return new int[] {i, index};
+      }
+    }
+    return new int[] {-1, -1};
+  }
 }
