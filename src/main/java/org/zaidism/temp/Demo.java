bbc347d44a897cf4a5e7cb20db0e35f5df4ee9d5
git-zaidisim <zaidisim7@gmail.com>
2024-02-28 22:41:26 +0530

    "duplicate char code with custom logic"

--- src/main/java/org/zaidism/temp/Demo.java
+++ src/main/java/org/zaidism/temp/Demo.java
@@ -1,27 +1,5 @@
 package org.zaidism.temp;
 
-import org.zaidism.model.Employee;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import static org.zaidism.model.EmployeeProvider.getEmployeeList;
-
 public class Demo {
-  public static void main(String[] args) {
-    List<Employee> employees = getEmployeeList();
-    HashMap<Integer, Integer> numsMap = new HashMap<>();
-    numsMap.put(1, 100);
-    numsMap.put(2, 200);
-    numsMap.put(3, 300);
-
-
-    for (Map.Entry<Integer, Integer> value : numsMap.entrySet()) {
-      System.out.println(value.getKey());
-      System.out.println(value.getValue());
-    }
-
-
-  }
+  public static void main(String[] args) {}
 }
