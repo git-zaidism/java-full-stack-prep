7610022a34dfee2aa351273705349513ce2ba599
git-zaidism <zaidism7@gmail.com>
2024-03-20 13:08:48 +0530

    "code and question updates"

--- src/main/java/org/zaidism/interviewprograms/StringDelimeter.java
+++ src/main/java/org/zaidism/interviewprograms/StringDelimeter.java
@@ -8,14 +8,20 @@ package org.zaidism.interviewprograms;
 // jahi d
 
 public class StringDelimeter {
-    public static void main(String[] args) {
-        String input = "jahid";
+  public static void main(String[] args) {
+    String input = "jahid";
 
-        for (int i = 1; i < input.length(); i++) {
-            StringBuilder output = new StringBuilder(input);
-            output.insert(i, ' ');
-            System.out.println(output);
-        }
+    for (int i = 1; i < input.length(); i++) {
+      StringBuilder output = new StringBuilder(input);
+      output.insert(i, ' ');
+      System.out.println(output);
     }
-}
+    System.out.println("-------------------------------------------------------------------------");
 
+    // another way
+    for (int i = 0; i <= input.length() - 1; i++) {
+      if (i != 0)
+        System.out.println(input.substring(0, i) + " " + input.substring(i, input.length()));
+    }
+  }
+}
