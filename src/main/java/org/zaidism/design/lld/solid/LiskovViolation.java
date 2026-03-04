0ac564f96a22c18fa78936719b2a18f4f9a427aa
Zahid <68467966+git-zaidism@users.noreply.github.com>
2026-03-04 14:26:21 +0530

    Refactor shipment classes into notification classes

--- src/main/java/org/zaidism/design/lld/solid/LiskovViolation.java
+++ src/main/java/org/zaidism/design/lld/solid/LiskovViolation.java
@@ -1,41 +1,34 @@
 package org.zaidism.design.lld.solid;
 
-abstract class Shipment {
-    abstract void deliverToAddress(String address);
-    abstract void currentShipmentStatus();
+abstract class NotificationV2 {
+    abstract void sendNotification(String msg, String email);
 }
 
-class StandardShipment extends Shipment {
+class EmailNotificationV2 extends NotificationV2 {
 
     @Override
-    void deliverToAddress(String address) {
-        System.out.println("shipment delivered to address" + address);
-    }
-
-    @Override
-    void currentShipmentStatus() {
-        System.out.println("current shipment status is...");
+    void sendNotification(String msg, String email) {
+        System.out.println("Message sent to email" + msg + " "+email);
     }
 }
 
-class HoldAtLocationShipment extends Shipment {
+class WhatsAppNotificationV2 extends NotificationV2 {
 
     @Override
-    void deliverToAddress(String address) {
-        throw new UnsupportedOperationException("Cannot deliver to address. Shipment is Hold At Location.");
-    }
-
-    @Override
-    void currentShipmentStatus() {
-        System.out.println("current shipment status is...");
+    void sendNotification(String msg, String email) {
+        throw new UnsupportedOperationException("Email NOtification not supported");
     }
 }
 
 public class LiskovViolation {
+
     public static void main(String[] args) {
-        StandardShipment standardShipment = new StandardShipment();
-        HoldAtLocationShipment holdAtLocationShipment = new HoldAtLocationShipment();
-            standardShipment.deliverToAddress("Jan Street");
-        holdAtLocationShipment.deliverToAddress("Jan Street");
+        NotificationV2 emailNotification = new EmailNotificationV2();
+        emailNotification.sendNotification("email notification...", "jahidquhr3@gmail.com");
+
+        NotificationV2 whatsAppNotification = new WhatsAppNotificationV2();
+        whatsAppNotification.sendNotification("whats app notification..", "3478438974938");
     }
+
 }
+
